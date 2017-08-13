dd440eb96f0e44613b20e6040c5c981394a04674
Matthew Forsyth <matt@mojotech.com>
2017-08-12 21:30:28 -0400

    Playing sound!

--- App.js
+++ App.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableHighlight } from 'react-native';
-// import Sound from 'react-native-sound';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+} from 'react-native';
+import { Asset, Audio } from 'expo';
 
 export default class App extends React.Component {
+  playHello = async function() {
+    try {
+      const {
+        soundObject,
+        status,
+      } = await Audio.Sound.create(require('./assets/sounds/hello.mp3'), {
+        shouldPlay: true,
+      });
+      // Your sound is playing!
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   onPressImage = e => {
     console.log('clickHiImage');
+    this.playHello();
   };
 
   render() {
     let pic = {
-      uri: 'http://www.fluentu.com/blog/wp-content/uploads/2016/04/how-to-say-hello-in-different-languages.jpg',
+      uri:
+        'http://www.fluentu.com/blog/wp-content/uploads/2016/04/how-to-say-hello-in-different-languages.jpg',
     };
     return (
       <View style={styles.container}>
         <TouchableHighlight onPress={this.onPressImage}>
-        <Image
-          source={pic}
-          style={{ width: 193, height: 110 }}
-          />
+          <Image source={pic} style={{ width: 193, height: 110 }} />
         </TouchableHighlight>
-        <Text>Hello Ollie!  It's Daddy!</Text>
+        <Text>Hello Ollie! It's Daddy!</Text>
       </View>
     );
   }
